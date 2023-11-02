052c2def8c4920217f0a4cb3de61d91a0b7f1a05
Ranjit Balhara <rbalhara@logitech.com>
2023-11-02 09:03:32 +0530

    Session 2 changes
    Session 2 changes

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,13 @@
+<script>
+	import FooterLink from "./FooterLink.svelte";
+</script>
+
+<footer class="p-6 flex flex-wrap gap-6 justify-between bg-gray-200 text-white">
+    <div class="">&copy;2023 Logitech</div>
+    <div class="text-blue flex flex-wrap gap-6">
+        <FooterLink href="#">About</FooterLink> 
+        <FooterLink href="#">Contact</FooterLink>
+        <FooterLink href="#">Support</FooterLink>
+        <FooterLink href="#">Privacy</FooterLink>
+    </div>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,25 @@
 <script>
-    let rtl = false;
-    function toggleRtl(){
-        rtl = !rtl;
-    }
+	import Footer from "../lib/components/Footer.svelte";
+	import Header from "../lib/components/Header.svelte";
+	import PlpPageLayout from "../lib/components/PlpPageLayout.svelte";
+
+	let rtl = false;
+	let dir;
+
+	function toggleRtl() {
+		rtl = !rtl;
+	}
+
+	function getDir(foo) {
+		return foo ? 'rtl' : 'ltr';
+	}
+	$: dir = rtl ? 'rtl': 'ltr';
 </script>
+
 <button on:click={toggleRtl}>Toggle RTL</button>
- <div class="grid gap-6" dir={rtl ? 'rtl': 'ltr'}>
-    <header class="border p-4 gap-6 justify-between flex items-center">
-        <div class="font-bold uppercase text-24 text-blue">Logitech</div>
-        <div class=""><iconify-icon class="text-30" icon="mdi:cart-outline"></iconify-icon></div>
-    </header>
-    <main class="grid gap-6 md:grid-cols-12">
-        <div class="border p-6 md:col-span-3">Filters</div>
-        <div class="border p-6 md:col-span-9">
-            <h1 class="font-bold text-20">Main Content</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed gravida ligula. Fusce congue sem mi. Nam consectetur sit amet risus in pulvinar. Nam vehicula nunc vel purus semper, a mattis arcu pulvinar. Vestibulum ut erat at leo commodo posuere at nec metus. Curabitur efficitur justo eu gravida faucibus</p>
-        </div>
-    </main>
-    <footer class="p-6 flex flex-wrap gap-6 justify-between bg-gray-200 text-white">
-        <div class="">&copy;2023 Logitech</div>
-        <div class="text-blue flex flex-wrap gap-6">
-            <a href="#" class="hover:underline">About</a>
-            <a href="#">Contact</a>
-            <a href="#">Support</a>
-            <a href="#">Privacy</a>
-        </div>
-    </footer>
+
+ <div class="grid gap-6" dir={getDir(rtl)}>
+	<Header />
+	<PlpPageLayout />
+	<Footer />
  </div>
